5817c25bca5c805e8f6603be0304ae91d97a01ba
artem <homelesspravitas@gmail.com>
2025-11-11 13:06:43 +0600

    refactor: Move game loop into a separate script

---
+++ GameLoop.cs
@@ -0,0 +1,63 @@
+﻿public class GameLoop
+{
+    public void Run()
+    {
+        string input;
+
+        string[] commands = [
+            GameCommands.Attack,
+            GameCommands.Potion,
+            GameCommands.ChangeWeapon,
+            GameCommands.ChangePotion,
+            GameCommands.Status,
+            GameCommands.Exit
+        ];
+
+        while (true)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Enter the command:\n");
+
+            foreach (var command in commands)
+            {
+                Console.WriteLine(command);
+            }
+            Console.WriteLine();
+
+            input = Console.ReadLine();
+
+            switch (input)
+            {
+                case GameCommands.Attack:
+                    ShowMessage("You attacked the enemy");
+                    break;
+                case GameCommands.Potion:
+                    ShowMessage("You use the potion");
+                    break;
+                case GameCommands.ChangeWeapon:
+                    ShowMessage("You changed the weapon");
+                    break;
+                case GameCommands.ChangePotion:
+                    ShowMessage("You changed the potion");
+                    break;
+                case GameCommands.Status:
+                    ShowMessage("Your stats will be here");
+                    break;
+                case GameCommands.Exit:
+                    Console.Clear();
+                    return;
+                default:
+                    ShowMessage("Wrong Command");
+                    break;
+            }
+        }
+    }
+
+    private void ShowMessage(string message)
+    {
+        Console.Clear();
+        Console.WriteLine(message);
+        Console.ReadLine();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,62 +2,6 @@
 {
     static void Main(string[] args)
     {
-        string input;
-
-        string[] commands = [
-            GameCommands.Attack,
-            GameCommands.Potion,
-            GameCommands.ChangeWeapon,
-            GameCommands.ChangePotion,
-            GameCommands.Status,
-            GameCommands.Exit
-        ];
-
-        while (true)
-        {
-            Console.Clear();
-
-            Console.WriteLine("Enter the command:\n");
-
-            foreach (var command in commands)
-            {
-                Console.WriteLine(command);
-            }
-            Console.WriteLine();
-
-            input = Console.ReadLine();
-
-            switch (input)
-            {
-                case GameCommands.Attack:
-                    ShowMessage("You attacked the enemy");
-                    break;
-                case GameCommands.Potion:
-                    ShowMessage("You use the potion");
-                    break;
-                case GameCommands.ChangeWeapon:
-                    ShowMessage("You changed the weapon");
-                    break;
-                case GameCommands.ChangePotion:
-                    ShowMessage("You changed the potion");
-                    break;
-                case GameCommands.Status:
-                    ShowMessage("Your stats will be here");
-                    break;
-                case GameCommands.Exit:
-                    Console.Clear();
-                    return;
-                default:
-                    ShowMessage("Wrong Command");
-                    break;
-            }
-        }
-    }
-
-    static void ShowMessage(string message)
-    {
-        Console.Clear();
-        Console.WriteLine(message);
-        Console.ReadLine();
+        new GameLoop().Run();
     }
 }
