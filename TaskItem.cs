2463d8609ecf3afa2960cf289b43de6cb7d5bbfd
artem <homelesspravitas@gmail.com>
2025-11-28 11:55:19 +0600

    feat: Add TaskItem.cs

---
+++ TaskItem.cs
@@ -0,0 +1,23 @@
+﻿internal class TaskItem
+{
+    private string _name;
+    private string _description;
+    private Priority _priority;
+    private Category _category;
+    private Status _status;
+
+    public TaskItem(string name, string description, Priority priority, Category category, Status status)
+    {
+        _name = name;
+        _description = description;
+        _priority = priority;
+        _category = category;
+        _status = status;
+    }
+
+    public string Name => _name;
+    public string Description => _description;
+    public Priority Priority => _priority;
+    public Category Category => _category;
+    public Status Status => _status;
+}
