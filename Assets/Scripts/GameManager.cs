75939d39a44d0cb70df1325ec6c9bc63bd5a3632
artem <homelesspravitas@gmail.com>
2025-12-12 13:58:46 +0600

    feat: Now Target spawns in random position

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject _targetPrefab;
-    void Start()
+    [SerializeField] private float _spawnInterval;
+    [SerializeField] private float _distanceFromCamera;
+    [Range(0f, 0.5f)]
+    [SerializeField] private float _margin;
+
+    private void Start()
     {
-        InvokeRepeating(nameof(SpawnTarget), 0, 4);
+        InvokeRepeating(nameof(SpawnTarget), 0, _spawnInterval);
     }
 
     private void SpawnTarget()
     {
-        GameObject.Instantiate(_targetPrefab);
+        float randomX = Random.Range(_margin, 1f - _margin);
+        float randomY = Random.Range(_margin, 1f - _margin);
+
+        Vector3 position = Camera.main.ViewportToWorldPoint(new Vector3(randomX, randomY, _distanceFromCamera));
+
+        GameObject.Instantiate(_targetPrefab, position, Quaternion.identity);
     }
 }
