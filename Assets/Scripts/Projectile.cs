095d420fd4d34137d60332e913417aa5a3738612
artem <homelesspravitas@gmail.com>
2025-12-24 17:03:33 +0600

    feat: Now projectile self-destructs over time

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] private float _lifetime;
+
+    private void Start()
+    {
+        Destroy(gameObject, _lifetime);
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Target"))
