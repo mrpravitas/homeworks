09535357eda620ae9d5272dc6a4b5101ec287a7d
artem <homelesspravitas@gmail.com>
2025-12-24 22:10:42 +0600

    feat: Add delay between shots

--- Assets/Scripts/Cannon/CannonShooter.cs
+++ Assets/Scripts/Cannon/CannonShooter.cs
@@ -9,18 +9,26 @@ public class CannonShooter : MonoBehaviour
     [SerializeField] private Transform _shotPoint;
 
     private int _shotCount = 0;
+    private float _timer = 0f;
 
     public UnityEvent<int> OnShooted;
 
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+    }
+
     public void OnShoot(InputAction.CallbackContext callbackContext)
     {
-        if (callbackContext.started)
+        if (callbackContext.started && _timer <= 0f)
         {
             GameObject projectile = Instantiate(_projectilePrefab, _shotPoint.position, _shotPoint.rotation);
             projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * _config.ShotForce);
 
             _shotCount++;
             OnShooted.Invoke(_shotCount);
+
+            _timer = _config.ReloadTime;
         }
     }
 }
