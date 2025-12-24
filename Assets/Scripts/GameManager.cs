b34dc3a345c7ae3c74a39840c41b7a9fab0636a7
artem <homelesspravitas@gmail.com>
2025-12-24 16:27:42 +0600

    feat: Add easy mode for testing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private float _distanceFromCamera;
     [Range(0f, 0.5f)]
     [SerializeField] private float _margin;
+    [Header("For testing")]
+    [SerializeField] private bool _easyMode;
 
     private int _targetCount;
 
@@ -20,18 +22,27 @@ public class GameManager : MonoBehaviour
 
     private void SpawnTarget()
     {
+        if (_easyMode)
+        {
+            Instantiate(GetRandomTarget(), Vector3.zero, Quaternion.identity);
+            return;
+        }
+
         float randomX = Random.Range(_margin, 1f - _margin);
         float randomY = Random.Range(_margin, 1f - _margin);
 
         Vector3 position = Camera.main.ViewportToWorldPoint(new Vector3(randomX, randomY, _distanceFromCamera));
 
-        GameObject target = _targetPrefabs[Random.Range(0, _targetPrefabs.Length)];
-
-        GameObject.Instantiate(target, position, Quaternion.identity);
+        Instantiate(GetRandomTarget(), position, Quaternion.identity);
 
         _targetCount++;
         OnTargetSpawned.Invoke(_targetCount);
 
         Debug.Log($"{_targetCount} targets have been spawned");
+    }
+
+    private GameObject GetRandomTarget()
+    {
+        return _targetPrefabs[Random.Range(0, _targetPrefabs.Length)];
     }
 }
