032e960090f040b4179b46fa2a910e5219639f7c
artem <homelesspravitas@gmail.com>
2026-01-16 23:39:53 +0600

    feat: Add exit button in main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,4 +7,13 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Game");
     }
+
+    public void Exit()
+    {
+        Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
 }
