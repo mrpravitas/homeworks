ccde35cb0dbc00159b12fbd44505eabeddd0dbc0
mrpravitas <homelesspravitas@gmail.com>
2026-02-08 17:15:27 +0600

    feat: Add NewsItem and NewsLoader

---
+++ Assets/Scripts/NewsItem.cs
@@ -0,0 +1,9 @@
+using System;
+
+[Serializable]
+public class NewsItem
+{
+    public string title;
+    public string content;
+    public DateTime timestamp;
+}
---
+++ Assets/Scripts/NewsLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class NewsLoader
+{
+    private string _filePath;
+    private List<NewsItem> _news;
+
+    public NewsLoader(string filePath)
+    { 
+        _filePath = filePath;
+    }
+
+    public async Task<List<NewsItem>> LoadNewsAsync()
+    {
+        string json = await File.ReadAllTextAsync(_filePath);
+        string wrapped = "{\"Items\":" + json + "}";
+
+        NewsWrapper wrappedNews = JsonUtility.FromJson<NewsWrapper>(wrapped);
+
+        return wrappedNews.Items;
+    }
+
+    [Serializable]
+    private class NewsWrapper
+    {
+        public List<NewsItem> Items;
+    }
+}
