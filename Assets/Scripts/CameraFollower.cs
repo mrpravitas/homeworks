65434b23b2d170f19fa9a8174815e88d0c0d6a03
artem <homelesspravitas@gmail.com>
2026-04-09 12:02:54 +0600

    feat: Add player movement

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    private Transform _camera;
+
+    private void Awake()
+    {
+        _camera = transform;
+    }
+
+    private void LateUpdate()
+    {
+        _camera.position = new Vector3(_target.position.x, _camera.position.y, _target.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private GameConfig _gameConfig;
+
+    private Vector3 _direction;
+    private Transform _transform;
+
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        _direction = new Vector3(horizontalInput, 0, verticalInput).normalized;
+        _transform.position += _direction * (Time.deltaTime * _gameConfig.PlayerSpeed);
+    }
+}
