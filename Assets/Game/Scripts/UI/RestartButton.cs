1ad8726fb54d13361e82ed35875119346f575f8c
artem <homelesspravitas@gmail.com>
2026-06-09 03:00:15 +0600

    feat: Add restart button

---
+++ Assets/Game/Scripts/UI/RestartButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartButton : MonoBehaviour
+{
+    [SerializeField] private Button _button;
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(RestartScene);
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(RestartScene);
+    }
+
+    private void RestartScene()
+    {
+        Scene curentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(curentScene.buildIndex);
+    }
+}
