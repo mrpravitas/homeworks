8cbe4a49c53dcab691a542fe0062b4904cad5f62
artem <homelesspravitas@gmail.com>
2026-06-08 12:52:09 +0600

    feat: Add player movement

---
+++ Assets/Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+
+    private Rigidbody _rigidbody;
+    private Vector3 _input;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = _rigidbody.position + _input * (_movementSpeed * Time.fixedDeltaTime);
+        _rigidbody.MovePosition(newPosition);
+    }
+}
